299c8db92b70b1d1b1c2159a489c3a34e1122fa2
Neil <neiloommen@hotmail.com>
2023-12-03 21:27:28 +1100

    Photoshoot images added

---
+++ page-photoshoot.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+<div class="container">
+<div class="container">
+            <div class="row col-12 justify-content-between justify-content-md-between justify-content-sm-center m-0">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/1.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/2.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/3.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/4.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/5.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/6.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/7.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/8.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/9.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/10.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/11.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/12.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/13.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/14.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/15.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/15_0.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/16.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/17.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/18.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/19.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/20.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/21.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/22.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/23.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/24.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/25.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/26.webp' ?>" alt="">
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/27.webp' ?>" alt="">            
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/28.webp' ?>" alt="">            
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/29.webp' ?>" alt="">            
+                <img class="project-image-gallary-item" src="<?php echo get_template_directory_uri() . '/assets/img/PhotoShoots/30.webp' ?>" alt="">            
+            </div>
+            </div>
+</div>
+<?php get_footer(); ?>
